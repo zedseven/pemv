7cc5786e4ce10453a244ccbda2b0430ca93c6245
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2022-10-25 04:54:02 -0400

    test: Add testing for automatic TLV parsing.

--- src/emv/tlv_parsing/auto_tlv.rs
+++ src/emv/tlv_parsing/auto_tlv.rs
@@ -32,3 +32,74 @@ pub fn parse(
 		Err(ParseError::Unrecognised)
 	}
 }
+
+// Unit Tests
+#[cfg(test)]
+mod tests {
+	// Uses
+	use super::{
+		super::{DataObjectType, EmvData, RawEmvBlock, RawEmvNode, RawEmvTag, TagClass, TlvFormat},
+		parse,
+	};
+	use crate::error::ParseError;
+
+	// Tests
+	#[allow(clippy::needless_pass_by_value)]
+	fn test_parse(data: &str, expected: Result<(TlvFormat, RawEmvBlock), ParseError>) {
+		let result = parse(data, ['*'].as_slice());
+		assert_eq!(expected, result);
+	}
+
+	#[test]
+	fn parse_empty_data() {
+		test_parse("", Err(ParseError::Unrecognised));
+	}
+	#[test]
+	fn parse_ber_tlv_valid() {
+		test_parse(
+			"5F2403251231",
+			Ok((
+				TlvFormat::BerTlv,
+				RawEmvBlock {
+					nodes: vec![RawEmvNode {
+						tag: RawEmvTag {
+							tag: vec![0x5F, 0x24],
+							class: TagClass::Application,
+							data_object_type: DataObjectType::Primitive,
+							data: EmvData::Normal(vec![0x25, 0x12, 0x31]),
+						},
+						child_block: RawEmvBlock::default(),
+					}],
+				},
+			)),
+		);
+	}
+	#[test]
+	fn parse_ber_tlv_invalid() {
+		test_parse("5F24032531", Err(ParseError::Unrecognised));
+	}
+	#[test]
+	fn parse_ingenico_valid() {
+		test_parse(
+			"T5F24:03:h251231",
+			Ok((
+				TlvFormat::Ingenico,
+				RawEmvBlock {
+					nodes: vec![RawEmvNode {
+						tag: RawEmvTag {
+							tag: vec![0x5F, 0x24],
+							class: TagClass::Application,
+							data_object_type: DataObjectType::Primitive,
+							data: EmvData::Normal(vec![0x25, 0x12, 0x31]),
+						},
+						child_block: RawEmvBlock::default(),
+					}],
+				},
+			)),
+		);
+	}
+	#[test]
+	fn parse_ingenico_invalid() {
+		test_parse("T5F24:03:h2531", Err(ParseError::Unrecognised));
+	}
+}
