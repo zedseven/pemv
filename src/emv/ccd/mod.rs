83e0b7a66b6bfddf61de237151959ab8d2cb2724
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2022-10-25 04:19:40 -0400

    doc(internal): Add documentation about where to find the specifications for the CCD module.

--- src/emv/ccd/mod.rs
+++ src/emv/ccd/mod.rs
@@ -1,3 +1,8 @@
+//! The EMV Common Core Definitions (CCD) values. Not all issuers comply with
+//! these, hence why they're in their own section.
+//!
+//! Information for this can be found in EMV Book 3, under `Part V`.
+
 // Modules
 mod cci;
 mod cvr;
